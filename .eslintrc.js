cf22128c49732febedee8c82ebc4dfb44cfb7c1b
nobrain711 <qa9975@ync.ac.kr>
2023-04-26 11:50:54 +0900

    edit setting

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,22 +4,49 @@ module.exports = {
     node: true,
   },
   extends: [
-    "plugin:vue/essential",
-    "eslint:recommended",
-    "plugin:prettier/recommended",
+    'plugin:vue/essential',
+    'eslint:recommended',
+    'plugin:prettier/recommended',
   ],
   parserOptions: {
-    parser: "@babel/eslint-parser",
+    parser: '@babel/eslint-parser',
   },
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    'no-console':
+      process.env.NODE_ENV === 'production'
+        ? 'warn'
+        : 'off',
+    'no-debugger':
+      process.env.NODE_ENV === 'production'
+        ? 'warn'
+        : 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        tabWidth: 2,
+        useTabs: false,
+        singleQuote: true,
+        jsxSingleQuote: true,
+        trailingComma: 'all',
+        bracketSameLine: true,
+        jsxBracketSameLine: true,
+        vueIndentScriptAndStyle: true,
+        printWidth: 60,
+      },
+    ],
+    'vue/multi-word-component-names': [
+      'error',
+      {
+        ignores: ['Header', 'Footer', 'temp'],
+      },
+    ],
+    'no-unused-vars': 'off',
   },
   overrides: [
     {
       files: [
-        "**/__tests__/*.{j,t}s?(x)",
-        "**/tests/unit/**/*.spec.{j,t}s?(x)",
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)',
       ],
       env: {
         jest: true,
